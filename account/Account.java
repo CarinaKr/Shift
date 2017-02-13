a201543ac5f09353d97b09af1317a222167ceca3
SvenMaderer <Sven.Maderer@haw-hamburg.de>
2017-02-13 21:27:05 +0100

    Oh ... forgot one

--- account/Account.java
+++ account/Account.java
@@ -1,6 +1,6 @@
 package account;
 
-public class Account {
+public class Account implements Comparable<Account> {
 	
 	private String userName;
 	private String password;
@@ -9,7 +9,7 @@ public class Account {
 	public Account(String name, String password){
 		this.userName = name;
 		this.password = password;
-		this.time = 1.5;
+		this.time = 0;
 	}
 	
 	public String getName(){
@@ -19,4 +19,31 @@ public class Account {
 	public double getTime(){
 		return this.time;
 	}
+	
+	public void setTime(double newTime){
+		this.time = newTime;
+	}
+	
+	public void setName(String newName){
+		this.userName = newName;
+	}
+	
+	public void setPassword(String newPassword){
+		this.password = newPassword;
+	}
+
+	@Override
+	public int compareTo(Account other) {
+		// TODO Auto-generated method stub
+		if(this.time < other.getTime()){
+			return 1;
+		}
+		else if(this.time > other.getTime()){
+			return -1;
+		}
+		else return 0;
+		
+	}
+	
+	
 }
